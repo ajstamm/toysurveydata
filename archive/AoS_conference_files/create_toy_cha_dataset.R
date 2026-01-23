71e7ec20f1fa6a4c207a6f7afdf5ce47e9fe1dd4
Abigail Stamm <13263174+ajstamm@users.noreply.github.com>
2026-01-23 15:31:05 -0600

    Add error function for county values

--- archive/AoS_conference_files/create_toy_cha_dataset.R
+++ archive/AoS_conference_files/create_toy_cha_dataset.R
@@ -2,7 +2,7 @@
 
 # set up ----
 # load data settings
-data_path <- "create_mock_data/data" # or whatever your path is
+data_path <- "archive/AoS_conference_files" # or whatever your path is
 report_count <- 200
 
 # read probabilities and functions ----
@@ -17,15 +17,22 @@ l <- list()
 
   # single column categorical ----
 metro <- c("Hennepin", "Ramsey", "Dakota", "Anoka", "Scott", "Carver", "Chisago")
-l$select_one <- select_categorical_set(d = vars, count = report_count) |>
+l$select_one <- select_categorical_set(d = vars, count = report_count)
+
+# deliberately add more error
+l$select_one <- l$select_one |>
   dplyr::mutate(
-    res_check = sample(0:2, prob = c(50, 10, 1), replace = TRUE, report_count),
+    reg_check = sample(0:2, prob = c(50, 10, 1), replace = TRUE, report_count),
     res_region = ifelse(res_county %in% metro, "Urban", "Rural"),
-    res_region = ifelse(is.na(res_county), NA, 
-                 ifelse(res_check == 0, res_region, 
-                 ifelse(res_check == 1, "I don't know", 
-                 ifelse(res_region == "Urban", "Rural", "Urban"))))
-  ) |> dplyr::select(-res_check)
+    res_region = dplyr::case_when(is.na(res_county) ~ NA,
+                                  reg_check == 0 ~ res_region,
+                                  reg_check == 1 ~ "I don't know",
+                                  res_region == "Urban" ~ "Rural",
+                                  .default = "Urban"),
+    res_county = sampling_error(res_county, type = "character")
+  ) |> dplyr::select(-reg_check)
+  
+  
   
   # numeric, IP, and dates ----
 c <- select_continuous_set(d = vars, count = report_count) |>
@@ -37,6 +44,7 @@ l$continuous <- dplyr::full_join(c, d, by = "id") |>
   dplyr::mutate(age = sample_age(start_date = c$birth_date, error = TRUE, 
                                  end_date = d$survey_date)) |>
   dplyr::select(-birth_date)
+
 l$ip <- sample_ip(report_count)
 
 
@@ -58,6 +66,7 @@ l$text <- data.frame(id = 1:report_count,
 data <- purrr::reduce(l, dplyr::left_join, by = "id") |> 
   dplyr::arrange(id)
 
+# add a duplicate record
 t <- data[sample(1:report_count, size = 1),] |>
   dplyr::mutate(id = max(data$id) + 1)
 
